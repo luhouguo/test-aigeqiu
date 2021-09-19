a92c1e9ad1c6070611a216f01fda902b9a0a3195
luhouguo <luhouguo@126.com>
2021-09-19 22:32:23 +0800

    Update funcs.c

--- lib/funcs.c
+++ lib/funcs.c
@@ -13,6 +13,9 @@ int testll()
     printf("testtesttestHello, World!\n");
     printf("testtesttestHello, World!\n");
     printf("testtesttestHello, World!testtesttestHello\n");
+    printf("testtesttestHello, World!testtesttestHello\n");
+    printf("testtesttestHello, World!testtesttestHello\n");
+    printf("testtesttestHello, World!testtesttestHello\n");
 
 
     return 0;
