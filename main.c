2b2064771595e1a5e2bab8491eb8cc3e9fe12d2d
luhouguo <luhouguo@126.com>
2021-09-19 22:01:06 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+
+
+int test()
+{
+    printf("testtesttesttesttesttesttest\n");
+    return 0;
+}
 int main() {
+    test();
     printf("Hello, World!\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
