893656091e91a99576c0b9efdac7c10be84a0e60
luhouguo <luhouguo@126.com>
2021-09-19 22:07:29 +0800

    Create funcs.c

---
+++ lib/funcs.c
@@ -0,0 +1,15 @@
+//
+// Created by luhouguo on 2021/9/19.
+//
+
+#include "funcs.h"
+
+int testll()
+{
+    printf("testtesttestHello, World!\n");
+    printf("testtesttestHello, World!\n");
+    printf("testtesttestHello, World!\n");
+    printf("testtesttestHello, World!\n");
+    printf("testtesttestHello, World!\n");
+    return 0;
+}
