f5b6e8a5f641ac968c42c4b2ed2667aee2e403a5
luhouguo <luhouguo@126.com>
2021-09-19 22:07:26 +0800

    Create funcs.h

---
+++ lib/funcs.h
@@ -0,0 +1,15 @@
+//
+// Created by luhouguo on 2021/9/19.
+//
+
+#ifndef TEST_AIGEQIU_FUNCS_H
+#define TEST_AIGEQIU_FUNCS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <strings.h>
+
+
+int testll();
+#endif //TEST_AIGEQIU_FUNCS_H
